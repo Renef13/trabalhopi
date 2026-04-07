f74918c0ca1e58cf55fc661bdfafcf2bbed405e7
Renef <renefricardo30@gmail.com>
2026-04-07 15:28:03 -0300

    add soma, subtracao , divisao e multiplicacao

---
+++ divisao_escalar.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+static int clamp_int(int v) {
+    return v < 0 ? 0 : (v > 255 ? 255 : v);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        return 1;
+    }
+
+    const char *nome_arquivo = argv[1];
+    double divisor = atof(argv[2]);
+
+    if (divisor == 0.0) {
+        printf("nao eh possivel dividir por zero.\n");
+        return 1;
+    }
+
+    int largura = 0, altura = 0, canais = 0;
+    unsigned char *imagem = stbi_load(nome_arquivo, &largura, &altura, &canais, 0);
+    if (!imagem) {
+        printf("nao foi possivel carregar a imagem '%s'.\n", nome_arquivo);
+        return 1;
+    }
+
+    int total_pixels = largura * altura * canais;
+    for (int i = 0; i < total_pixels; i++) {
+        int novo_valor = (int) (imagem[i] / divisor);
+        imagem[i] = (unsigned char) clamp_int(novo_valor);
+    }
+
+    if (stbi_write_jpg("resultado_divisao.jpg", largura, altura, canais, imagem, 100)) {
+        printf("imagem salva 'resultado_divisao.jpg'\n");
+    } else {
+        printf("nao foi possivel salvar.\n");
+        stbi_image_free(imagem);
+        return 1;
+    }
+
+    stbi_image_free(imagem);
+    return 0;
+}
